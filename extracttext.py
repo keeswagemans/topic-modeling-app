3efd57af78d1aa38b7a6dae6d07f3b6f2a153e38
Kees Wagemans <wagemans.kees@gmail.com>
2024-12-10 12:33:09 +0100

    Changed extracttest and preprocess

--- extracttext.py
+++ extracttext.py
@@ -2,20 +2,12 @@ import os
 import json 
 from PyPDF2 import PdfReader
 from docx import Document
+import pymupdf   
 
 class ExtractText:
 
-    def __init__(self, path): 
-        """
-        Initialize the ExtractText class.
-
-        Args:
-            path (str): Path to the directory or file.
-            output_data (dict): A dictionary to store extracted text data.
-        """
-        self.path = path  
-
-    def extract_text_from_files(self, path):
+    @staticmethod 
+    def extract_text_from_files(path):
         """
         Extracts text from files in directory.
         """
@@ -23,19 +15,26 @@ class ExtractText:
         _, ext = os.path.splitext(path)
 
         if ext == ".pdf":
+            # try:
+            #     with open(path, 'rb') as file:
+            #         reader = PdfReader(file)
+            #         for page in reader.pages:
+            #             text += page.extract_text()
+            # except Exception as e:
+            #     print(f"Error reading PDF {path}: {e}")
             try:
-                with open(path, 'rb') as file:
-                    reader = PdfReader(file)
-                    for page in reader.pages:
-                        text += " " + page.extract_text() + " "
-            except Exception as e:
-                print(f"Error reading PDF {path}: {e}")
+                doc = pymupdf.open(path)
+                for page in doc: 
+                    text += page.get_text()
+            except Exception as e: 
+                print(f"Error reading PDF")
+
 
         elif ext == ".docx":
             try:
                 doc = Document(path)
                 for para in doc.paragraphs:
-                    text += " " + para.text + " "
+                    text += para.text + "\n"
             except Exception as e:
                 print(f"Error reading DOCX {path}: {e}")
 
@@ -44,7 +43,8 @@ class ExtractText:
 
         return text
     
-    def extract_from_directory(self, path):
+    @staticmethod 
+    def extract_from_directory(path):
         count = 0
 
         all_text = {}
@@ -54,13 +54,12 @@ class ExtractText:
                 count += 1
                 print(count)
                 file_path = os.path.join(root, file_name)
-                file_text = self.extract_text_from_files(file_path)
+                file_text = ExtractText.extract_text_from_files(file_path)
                 file_text = str(file_text)
                 all_text[file_name] = file_text
                 print(file_name)
-        return all_text
+        return all_text 
 
 # Run the code 
-instance = ExtractText("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/documenten/")
-output_data = instance.extract_from_directory("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/documenten/")
+output_data = ExtractText.extract_from_directory("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/documenten/")
 json.dump(output_data, open("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/extractedtext/extractedtext.json", "w"))
--- preprocess.py
+++ preprocess.py
@@ -6,13 +6,12 @@ import spacy
 import json 
 
 class Preprocess:
-    
-    def __init__(self, input_data): 
-        self.input_data = input_data
 
-    def preprocessing(self, text_list):
+    @staticmethod
+    def preprocessing(text_list):
         """
         Regular preprocessing steps for a list of strings.
+
         """
         dictionary = {}
         url_pattern = re.compile(r'https?://\S+|www\.\S+')
@@ -20,31 +19,33 @@ class Preprocess:
 
         for key, value in text_list.items():
             text_data_cleaned = []
-            for text in value:
-                text = re.sub(url_pattern, '', text)
-                text = re.sub(punctuation_pattern, '', text)
-                text = re.sub(r'\s-\s', ' ', text)
-                text = text.strip()
-                text = text.replace('\\n', '').replace('\n', '')
-                text = re.sub(r'[^a-zA-Z\s]', '', text)
-                text = text.lower()
+            print(value)
+            words = value.split()
+            for word in words:
+                word = re.sub(url_pattern, '', word)
+                word = re.sub(punctuation_pattern, '', word)
+                word = re.sub(r'\s-\s', ' ', word)
+                word = word.strip()
+                word = word.replace('\\n', '').replace('\n', '')
+                word = re.sub(r'[^a-zA-Z\s]', '', word)
+                word = word.lower()
 
-                if text:
-                    text_data_cleaned.append(text)
+                if word:
+                    text_data_cleaned.append(word)
 
             dictionary[key] = text_data_cleaned
 
         return dictionary
-    
-    def remove_stopwords(self, text, language):
+
+    @staticmethod
+    def remove_stopwords(text, language):
         """
         Removing stopwords from text.
-
         """
         dictionary = {}
 
         nltk.download('stopwords')
-        stop_words = (stopwords.words(language))
+        stop_words = stopwords.words(language)
 
         for key, value in text.items():
             filtered_text = []
@@ -52,20 +53,27 @@ class Preprocess:
                 if word not in stop_words:
                     filtered_text.append(word)
 
-        dictionary[key] = filtered_text
+            dictionary[key] = filtered_text
 
         return dictionary
-    
-    def remove_filterwords(self, text):
-        """
-        Removing Dutch words.
 
+    @staticmethod
+    def remove_filterwords(text):
+        """
+        Removing Dutch words based on a predefined filter list.
         """
         dictionary = {}
 
-        df_tsv = pd.read_csv('C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/molex/molex_22_02_2022.tsv', delimiter='\t', engine='python')
-        filterwords = df_tsv[df_tsv['NOU-C(number=sg)'].str.contains("ADV|PD|COLL|AA|ADP|RES|INT|NOU-P|NUM|VRB|CONJ", case=False, na=False)]['1 aprilmopje'].tolist()
-
+        df_tsv = pd.read_csv(
+            'C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/molex/molex_22_02_2022.tsv',
+            delimiter='\t', engine='python'
+        )
+        filterwords = df_tsv[
+            df_tsv['NOU-C(number=sg)'].str.contains(
+                "ADV|PD|COLL|AA|ADP|RES|INT|NOU-P|NUM|VRB|CONJ", 
+                case=False, na=False
+            )
+        ]['1 aprilmopje'].tolist()
 
         for key, value in text.items():
             filtered_text = []
@@ -73,14 +81,14 @@ class Preprocess:
                 if word not in filterwords:
                     filtered_text.append(word)
 
-        dictionary[key] = filtered_text
+            dictionary[key] = filtered_text
 
-        return dictionary 
+        return dictionary
 
-    def remove_custom_filterwords(self, text, custom):
+    @staticmethod
+    def remove_custom_filterwords(text, custom):
         """
-        Removing custom filterwords.
-
+        Removing custom filter words.
         """
         dictionary = {}
 
@@ -90,11 +98,12 @@ class Preprocess:
                 if word not in custom:
                     custom_filter_words_text.append(word)
 
-        dictionary[key] = custom_filter_words_text
+            dictionary[key] = custom_filter_words_text
 
         return dictionary
-    
-    def lemmatization(self, text):
+
+    @staticmethod
+    def lemmatization(text):
         """
         Returns the lemmatized text for each key in the input dictionary.
 
@@ -118,10 +127,9 @@ class Preprocess:
 
 # Running the code 
 input_data = json.load(open("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/extractedtext/extractedtext.json"))
-instance = Preprocess(input_data)
-data_preprocessed = instance.preprocessing(input_data)
-data_stopwords_dutch = instance.remove_stopwords(input_data, 'dutch')
-data_stopwords_english = instance.remove_stopwords(data_stopwords_dutch, 'english')
-data_filterwords = instance.remove_filterwords(data_stopwords_english)
-data_lemma = instance.lemmatization(data_filterwords)
+data_preprocessed = Preprocess.preprocessing(input_data)
+data_stopwords_dutch = Preprocess.remove_stopwords(data_preprocessed, 'dutch')
+data_stopwords_english = Preprocess.remove_stopwords(data_stopwords_dutch, 'english')
+data_filterwords = Preprocess.remove_filterwords(data_stopwords_english)
+data_lemma = Preprocess.lemmatization(data_filterwords)
 json.dump(data_lemma, open("C:/Users/KWAGEMAN/Documents/LDA_App/topicmodelingapp/preprocessing/preprocessing.json", "w"))
